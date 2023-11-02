c1c357de2b11452e4c3e25eb18d7fecfb479e356
Artem Krol <artemkrol47@gmail.com>
2023-11-02 06:56:17 -0500

    updated LoggingService

--- src/Net.Utils.CloudWatchHandler/Services/LoggingService.cs
+++ src/Net.Utils.CloudWatchHandler/Services/LoggingService.cs
@@ -2,46 +2,54 @@
 using Amazon.CloudWatchLogs.Model;
 using Net.Utils.CloudWatchHandler.Models;
 using Newtonsoft.Json;
+using InvalidOperationException = System.InvalidOperationException;
 
-namespace Net.Utils.CloudWatchHandler.Services;
-
-public class LoggingService
+namespace Net.Utils.CloudWatchHandler.Services
 {
-    private readonly IAmazonCloudWatchLogs _cloudWatchClient;
-    private readonly LogStreamService _logStreamService;
-    private string? _sequenceToken;
-
-    public LoggingService(IAmazonCloudWatchLogs cloudWatchClient, LogStreamService logStreamService)
+    public class LoggingService
     {
-        _cloudWatchClient = cloudWatchClient ?? throw new ArgumentNullException(nameof(cloudWatchClient));
-        _logStreamService = logStreamService ?? throw new ArgumentNullException(nameof(logStreamService));
-    }
+        private readonly IAmazonCloudWatchLogs _cloudWatchClient;
+        private readonly LogStreamService _logStreamService;
+        private string? _sequenceToken;
+        private const int MaxRetries = 3;
 
-    public virtual async Task LogMessageAsync(LogConfiguration logConfiguration)
-    {
-        var logEvent = new InputLogEvent
+        public LoggingService(IAmazonCloudWatchLogs cloudWatchClient, LogStreamService logStreamService)
         {
-            Timestamp = DateTime.UtcNow,
-            Message = JsonConvert.SerializeObject(logConfiguration.Details)
-        };
-
-        var request = new PutLogEventsRequest
-        {
-            LogGroupName = logConfiguration.LogGroupName,
-            LogStreamName = await _logStreamService.CreateLogStreamAsync(logConfiguration.Prefix, logConfiguration.StreamCreationIntervalInMinutes, logConfiguration.LogGroupName, logConfiguration.LogStreamName),
-            LogEvents = new List<InputLogEvent> { logEvent },
-            SequenceToken = _sequenceToken
-        };
-
-        try
-        {
-            var response = await _cloudWatchClient.PutLogEventsAsync(request);
-            _sequenceToken = response.NextSequenceToken;
+            _cloudWatchClient = cloudWatchClient ?? throw new ArgumentNullException(nameof(cloudWatchClient));
+            _logStreamService = logStreamService ?? throw new ArgumentNullException(nameof(logStreamService));
         }
-        catch (InvalidSequenceTokenException ex)
+
+        public virtual async Task LogMessageAsync(LogConfiguration logConfiguration)
         {
-            _sequenceToken = ex.ExpectedSequenceToken;
-            await LogMessageAsync(logConfiguration);
+            var logEvent = new InputLogEvent
+            {
+                Timestamp = DateTime.UtcNow,
+                Message = JsonConvert.SerializeObject(logConfiguration.Details)
+            };
+
+            var request = new PutLogEventsRequest
+            {
+                LogGroupName = logConfiguration.LogGroupName,
+                LogStreamName = await _logStreamService.CreateLogStreamAsync(logConfiguration.LogGroupName ?? throw new InvalidOperationException(), logConfiguration.LogStreamName ?? throw new InvalidOperationException()),
+                LogEvents = new List<InputLogEvent> { logEvent },
+                SequenceToken = _sequenceToken
+            };
+
+            var retryCount = 0;
+            while (retryCount < MaxRetries)
+            {
+                try
+                {
+                    var response = await _cloudWatchClient.PutLogEventsAsync(request);
+                    _sequenceToken = response.NextSequenceToken;
+                    break;
+                }
+                catch (InvalidSequenceTokenException ex)
+                {
+                    _sequenceToken = ex.ExpectedSequenceToken;
+                    retryCount++;
+                }
+            }
         }
     }
 }
