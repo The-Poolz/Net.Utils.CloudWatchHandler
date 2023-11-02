de30d21000b32b692d2fd3eba85a4f4643e3a652
Artem Krol <artemkrol47@gmail.com>
2023-11-02 06:57:57 -0500

    updated LogConfiguration

--- src/Net.Utils.CloudWatchHandler/Models/LogConfiguration.cs
+++ src/Net.Utils.CloudWatchHandler/Models/LogConfiguration.cs
@@ -7,19 +7,10 @@ public class LogConfiguration
     public LogConfiguration(string? prefix, int streamCreationIntervalInMinutes, string? logGroupName,
         MessageDetails? details, string logStreamName)
     {
-        Prefix = prefix;
-        StreamCreationIntervalInMinutes = streamCreationIntervalInMinutes;
         LogGroupName = logGroupName;
         LogStreamName = logStreamName;
         Details = details;
     }
-
-    [Required]
-    [MaxLength(100)]
-    public string? Prefix { get;  set; }
-
-    [Range(1, 1440)]
-    public int StreamCreationIntervalInMinutes { get;  set; }
 
     [Required]
     public string? LogGroupName { get; set; }
